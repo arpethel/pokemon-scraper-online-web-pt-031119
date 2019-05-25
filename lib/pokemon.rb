e06ae07278412a7dc0b94c2ff98e270432fb802b
Ryan Pethel <arpethel@gmail.com>
2019-05-25 16:10:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,13 +3,13 @@ require 'pry'
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id, name, type, db)
-    binding.pry
-    @id = id
-    @name = name
-    @type = type
-    @db = db
-  end
+  # def initialize(id, name, type, db)
+  #   binding.pry
+  #   @id = id
+  #   @name = name
+  #   @type = type
+  #   @db = db
+  # end
 
   # def save
   #   sql = <<-SQL
