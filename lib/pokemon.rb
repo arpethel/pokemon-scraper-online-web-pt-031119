05069f14f48d0d79d8814a3960fd6861deb1fdcf
Ryan Pethel <arpethel@gmail.com>
2019-05-25 16:52:43 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -42,10 +42,10 @@ class Pokemon
 
     db.execute(sql, id).map do |row|
       # binding.pry
-      id = row[0]
+      # id = row[0]
       # name = row[1]
       # type = row[2]
-      new_pokemon = self.new(id) #, name, type)
+      # new_pokemon = self.new(id) #, name, type)
       binding.pry
     end
   end
