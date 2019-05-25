8a791e69ddaed2ea27ee20fa380448cf188c715d
Ryan Pethel <arpethel@gmail.com>
2019-05-25 17:58:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -39,8 +39,14 @@ class Pokemon
       WHERE id = ?
       SQL
 
+
+
+# def self.find(id_num, db)     
+#   pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten 
+#       Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
+#    end
 # binding.pry
-      # db.execute(sql, id_num)#.each {|row| binding.pry Pokemon.new(id)}
+      new_pokemon = db.execute(sql, id_num)#.each {|row| binding.pry Pokemon.new(id)}
       # binding.pry
       # Pokemon.new(id, db)
       # id = row[0]
